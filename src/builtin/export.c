cc21cfa6dba46a686cd5befca420a378a9ff73d6
Houtworm <codam@houtworm.net>
2023-06-22 22:19:04 +0200

    fixed export existing entry

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   export.c                                        |o_o || |                */
+/*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: houtworm <codam@houtworm.net>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 18:12:44 by houtworm          #+#    #+#             */
-/*   Updated: 2023/05/29 23:07:54 by houtworm     \___)=(___/                 */
+/*   Updated: 2023/06/22 22:18:34 by houtworm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,19 @@ int	ft_export(t_shell *strct)
 	var = ft_calloc(512, 1);
 	val = ft_calloc(512, 1);
 	line = strct->line;
-	while (line[i + 6] != '\0')
+	while (line[i + 7] != '\0' && line[i + 7] != '=')
 	{
 		var[i] = line[7 + i];
 		i++;
-		if (line[7 + i] == '=')
-		{
-			i++;
-			break ;
-		}
 	}
-	ft_printf("%s\n", var);
-	while (line[i + j + 7] != ' ' && line[i + j + 7] != '\0')
+	if (line[i + 7] != '=')
+		return (1);
+	while (line[i + j + 8] != '\0')
 	{
-		val[j] = line[7 + i + j];
+		val[j] = line[8 + j + i];
 		j++;
 	}
 	ft_setenv(strct->envp, var, val);
-	ft_printf("%s\n", val);
 	free(var);
 	free(val);
 	return (0);
