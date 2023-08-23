6ef2e15208b137acdfb7001666829e2eeb68a652
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-23 09:39:20 +0200

    added some tests

--- test.sh
+++ test.sh
@@ -6,13 +6,14 @@
 #    By: djonker <djonker@student.codam.nl>         //   \ \ __| | | \ \/ /    #
 #                                                  (|     | )|_| |_| |>  <     #
 #    Created: 2023/08/23 06:35:52 by djonker      /'\_   _/`\__|\__,_/_/\_\    #
-#    Updated: 2023/08/23 09:32:59 by djonker      \___)=(___/                  #
+#    Updated: 2023/08/23 09:39:13 by djonker      \___)=(___/                  #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 ERRORS=0
+PASSES=0
 
 testfunction()
 {
@@ -24,6 +25,7 @@ testfunction()
 	if [ $? -eq 0 ]
 	then
 		printf "\e[1;32mstdout OK \e[0;00m"
+		PASSES=$(($PASSES+1))
 	else
 		printf "\e[1;31mKO stdout doesn't match with command ${@} \nreal: $(cat realstdoutfile 2> /dev/null)\nmini: $(cat ministdoutfile 2> /dev/null)\e[0;00m\n"
 		ERRORS=$(($ERRORS+1))
@@ -32,6 +34,7 @@ testfunction()
 	if [ $? -eq 0 ]
 	then
 		printf "\e[1;32mstderr OK \e[0;00m"
+		PASSES=$(($PASSES+1))
 	else
 		printf "\n\e[1;31mKO stderr doesn't match with command ${@} \nreal: $(cat realstderrfile 2> /dev/null)\nmini: $(cat ministderrfile 2> /dev/null)\e[0;00m\n"
 		ERRORS=$(($ERRORS+1))
@@ -42,6 +45,7 @@ testfunction()
 		ERRORS=$(($ERRORS+1))
 	else
 		printf "\e[1;32mreturn OK\n\e[0;00m"
+		PASSES=$(($PASSES+1))
 	fi
 	rm realstdoutfile realerroutfile ministdoutfile minierroutfile
 	sleep 0.2
@@ -109,5 +113,5 @@ testfunction "cd ~/Documents"
 testfunction "cd nonexistingpath"
 
 # Shutdown
-printf "\e[1;36mThe tester found $ERRORS KO\'s\e[0;00m\n"
+printf "\e[1;36mThe tester found $ERRORS KO\'s and $PASSES OK\'s\e[0;00m\n"
 exit $ERRORS
