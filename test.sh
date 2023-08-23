a68ac0d3ecd6aeb5e2164e1c136f304e9fdb0eb6
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-23 07:47:16 +0200

    Update Tester

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #    By: djonker <djonker@student.codam.nl>         //   \ \ __| | | \ \/ /    #
 #                                                  (|     | )|_| |_| |>  <     #
 #    Created: 2023/08/23 06:35:52 by djonker      /'\_   _/`\__|\__,_/_/\_\    #
-#    Updated: 2023/08/23 07:26:11 by djonker      \___)=(___/                  #
+#    Updated: 2023/08/23 07:47:15 by djonker      \___)=(___/                  #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,33 +21,32 @@ testfunction()
 	diff realstdoutfile ministdoutfile > /dev/null
 	if [ $? -eq 0 ]
 	then
-		printf "\e[1;32mOK \e[0;00m"
+		printf "\e[1;32mstdout OK \e[0;00m"
 	else
-		printf "\n\e[1;31mKO stdout doesn't match with command $@\e[0;00m\n"
-		printf "\nreal:\n"
-		cat realstdoutfile
-		printf "\nmini:\n"
-		cat ministdoutfile
+		printf "\n\e[1;31mKO stdout doesn't match with command ${@} \nreal: $(cat realstdoutfile)\nmini: $(cat ministdoutfile)\e[0;00m\n"
 	fi
 	diff realerroutfile minierroutfile > /dev/null
 	if [ $? -eq 0 ]
 	then
-		printf "\e[1;32mOK \e[0;00m"
+		printf "\e[1;32mstderr OK \e[0;00m"
 	else
-		printf "\n\e[1;31mKO stderr doesn't match with command $@\e[0;00m\n"
-		printf "\nreal:\n"
-		cat realerroutfile
-		printf "\nmini:\n"
-		cat minierroutfile
+		printf "\n\e[1;31mKO stderr doesn't match with command ${@} \nreal: $(cat realstderrfile)\nmini: $(cat ministderrfile)\e[0;00m\n"
 	fi
 	if [ $REALRETURN -ne $MINIRETURN ]
 	then
-		printf "\n\e[1;31mKO Return doesn't match with command $@\nReal $REALRETURN\nMini $MINIRETURN\e[0;00m\n"
+		printf "\n\e[1;31mKO Return doesn't match with command ${@} \nReal $REALRETURN\nMini $MINIRETURN\e[0;00m\n"
 	else
-		printf "\e[1;32mOK \e[0;00m"
+		printf "\e[1;32mreturn OK\n\e[0;00m"
 	fi
+	sleep 0.1
 	rm realstdoutfile realerroutfile ministdoutfile minierroutfile
 }
 
 testfunction echo hallo
+testfunction echo "hallo"
+testfunction echo 'hallo'
 testfunction echo -n hallo
+testfunction echo -nnn hallo
+testfunction echo -nnm hallo
+testfunction echo -n -n hallo
+
