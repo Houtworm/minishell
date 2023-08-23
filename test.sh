4ba9f56340b158ea4e03749c22d07cbb5132f2a2
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-23 06:56:46 +0200

    Update Readme

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #    By: djonker <djonker@student.codam.nl>         //   \ \ __| | | \ \/ /    #
 #                                                  (|     | )|_| |_| |>  <     #
 #    Created: 2023/08/23 06:35:52 by djonker      /'\_   _/`\__|\__,_/_/\_\    #
-#    Updated: 2023/08/23 06:35:53 by djonker      \___)=(___/                  #
+#    Updated: 2023/08/23 06:56:43 by djonker      \___)=(___/                  #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,23 +14,23 @@
 
 testfunction()
 {
-	REALOUTPUT=$(bash $1)
+	bash -c $@ > realstdoutfile
 	REALRETURN=$?
-	MINIOUTPUT=$(./minishell $1)
+	minishell -c $@ > ministdoutfile
 	MINIRETURN=$?
-	if [ $REALOUTPUT != $MINIOUTPUT ]
+	if [ "$REALOUTPUT" != "$MINIOUTPUT" ]
 	then
 		printf "\n\e[1;31mKO Output doesn't match\nReal $REALOUTPUT\nMini $MINIOUTPUT\e[0;00m\n"
 	else
-		printf "\e[1;32mOK \e[0;00m]]"
+		printf "\e[1;32mOK \e[0;00m"
 	fi
 	if [ $REALRETURN -ne $MINIRETURN ]
 	then
 		printf "\n\e[1;31mKO Return doesn't match\nReal $REALRETURN\nMini $MINIRETURN\e[0;00m\n"
 	else
-		printf "\e[1;32mOK \e[0;00m]]"
+		printf "\e[1;32mOK \e[0;00m"
 	fi
 }
 
-testfunction "echo hallo"
-testfunction "echo -n hallo"
+testfunction echo hallo
+testfunction echo -n hallo
