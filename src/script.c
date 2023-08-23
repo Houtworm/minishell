3858ff0b4e4af097eef8f91d2d6698d37fa89e09
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-23 08:37:02 +0200

    fixed script parser and added -c flag for a single command

--- src/script.c
+++ src/script.c
@@ -6,7 +6,7 @@
 /*   By: djonker <djonker@student.codam.nl>         //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2023/03/23 14:59:51 by djonker      /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/08/23 04:37:04 by djonker      \___)=(___/                 */
+/*   Updated: 2023/08/23 08:31:57 by djonker      \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,25 @@ int	ft_runscript(int argc, char **argv, char **envp)
 	i = 1;
 	ret = 1;
 	shell = ft_initstruct(envp);
-	while (i < argc)
+	if (ft_strncmp(argv[1], "-c", 1) == 0)
 	{
-		fd = open(argv[1], O_RDONLY);
-		while (ret > 0)
+		ft_parseline(argv[2], shell);
+		ft_executecommands(shell);
+	}
+	else
+	{
+		while (i < argc)
 		{
-			ret = get_next_line(fd, &line); // write second GNL for \ at end of line
-			ft_parseline(line, shell);
-			ft_executecommands(shell);
-			free(line);
+			fd = open(argv[1], O_RDONLY);
+			while (ret > 0)
+			{
+				ret = get_next_line(fd, &line); // write second GNL for \ at end of line
+				ft_parseline(line, shell);
+				ft_executecommands(shell);
+				free(line);
+			}
+			i++;
 		}
-		i++;
 	}
 	// cleanup
 	return (SUCCESS);
