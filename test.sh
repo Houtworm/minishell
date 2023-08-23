ecc4c7e72b704f8e1e4c625e8e8cdf22b6177958
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-23 08:18:06 +0200

    Update Tester

--- test.sh
+++ test.sh
@@ -6,11 +6,13 @@
 #    By: djonker <djonker@student.codam.nl>         //   \ \ __| | | \ \/ /    #
 #                                                  (|     | )|_| |_| |>  <     #
 #    Created: 2023/08/23 06:35:52 by djonker      /'\_   _/`\__|\__,_/_/\_\    #
-#    Updated: 2023/08/23 07:56:25 by djonker      \___)=(___/                  #
+#    Updated: 2023/08/23 08:11:27 by djonker      \___)=(___/                  #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
+
+ERRORS=0
 
 testfunction()
 {
@@ -24,6 +26,7 @@ testfunction()
 		printf "\e[1;32mstdout OK \e[0;00m"
 	else
 		printf "\n\e[1;31mKO stdout doesn't match with command ${@} \nreal: $(cat realstdoutfile)\nmini: $(cat ministdoutfile)\e[0;00m\n"
+		ERRORS=$(($ERRORS+1))
 	fi
 	diff realerroutfile minierroutfile > /dev/null
 	if [ $? -eq 0 ]
@@ -31,10 +34,12 @@ testfunction()
 		printf "\e[1;32mstderr OK \e[0;00m"
 	else
 		printf "\n\e[1;31mKO stderr doesn't match with command ${@} \nreal: $(cat realstderrfile)\nmini: $(cat ministderrfile)\e[0;00m\n"
+		ERRORS=$(($ERRORS+1))
 	fi
 	if [ $REALRETURN -ne $MINIRETURN ]
 	then
 		printf "\n\e[1;31mKO Return doesn't match with command ${@} \nReal $REALRETURN\nMini $MINIRETURN\e[0;00m\n"
+		ERRORS=$(($ERRORS+1))
 	else
 		printf "\e[1;32mreturn OK\n\e[0;00m"
 	fi
@@ -42,6 +47,9 @@ testfunction()
 	sleep 0.2
 }
 
+# Testing Lines
+
+# echo
 testfunction "echo hallo"
 testfunction 'echo "hallo"'
 testfunction "echo 'hallo'"
@@ -50,3 +58,16 @@ testfunction "echo -nnn hallo"
 testfunction "echo -nnm hallo"
 testfunction "echo -n -n hallo"
 
+# pwd
+
+# export
+
+# unset
+
+# env
+
+# exit
+
+# Shutdown
+printf "\e[1;36mThe tester found $ERRORS KO\'s\e[0;00m\n"
+exit $ERRORS
