067b34ecabcfe1992f61cabd6952f7c2388c4206
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-26 12:30:41 +0200

    echo working for single argument

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,27 +6,72 @@
 /*   By: houtworm <codam@houtworm.net>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 18:13:11 by houtworm          #+#    #+#             */
-/*   Updated: 2023/08/25 02:29:20 by houtworm     \___)=(___/                 */
+/*   Updated: 2023/08/26 12:30:18 by djonker      \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+/*int	ft_echo(t_cmds cmds)*/
+/*{*/
+	/*char	*print;*/
+	/*int		i;*/
+	/*int		j;*/
+	/*int		k;*/
+	/*int		l;*/
+
+	/*i = 0;*/
+	/*j = 0;*/
+	/*k = 0;*/
+	/*l = 0;*/
+	/*if (cmds.arguments[1])*/
+	/*{*/
+		/*print = ft_calloc(512, 1);*/
+		/*line = cmds.arguments[1];*/
+		/*if (line[i + j] == '-')*/
+		/*{*/
+			/*j++;*/
+			/*while (line[i + j + k] == 'n')*/
+				/*k++;*/
+			/*if (line[i + j + k] == ' ')*/
+				/*k++;*/
+			/*else*/
+			/*{*/
+				/*k = 0;*/
+				/*j--;*/
+			/*}*/
+		/*}*/
+		/*while (line[i + j + k] != '\0')*/
+		/*{*/
+			/*print[i] = line[j + i + k];*/
+			/*i++;*/
+		/*}*/
+		/*print[i] = '\0';*/
+		/*ft_printf("%s", print);*/
+		/*if (k == 0)*/
+			/*ft_printf("\n");*/
+	/*}*/
+	/*else*/
+		/*ft_printf("\n");*/
+	/*return (0);*/
+/*}*/
+
 int	ft_echo(t_cmds cmds)
 {
-	char	*line;
 	char	*print;
 	int		i;
 	int		j;
 	int		k;
+	int		l;
 
 	i = 0;
-	j = 5;
+	j = 0;
 	k = 0;
-	if (!ft_strncmp(cmds.arguments[0], "echo ", 5))
+	l = 0;
+	if (cmds.arguments[1])
 	{
 		print = ft_calloc(512, 1);
-		line = cmds.arguments[0];
+		line = cmds.arguments[1];
 		if (line[i + j] == '-')
 		{
 			j++;
@@ -34,8 +79,6 @@ int	ft_echo(t_cmds cmds)
 				k++;
 			if (line[i + j + k] == ' ')
 				k++;
-			/*else if	(line[i + j + k] == '\0')*/
-				/*k = k;*/
 			else
 			{
 				k = 0;
@@ -52,7 +95,7 @@ int	ft_echo(t_cmds cmds)
 		if (k == 0)
 			ft_printf("\n");
 	}
-	if (!ft_strncmp(cmds.arguments[0], "echo\0", 5))
+	else
 		ft_printf("\n");
 	return (0);
 }
