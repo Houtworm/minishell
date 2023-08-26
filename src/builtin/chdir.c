fd16e30766cd31c22ce4a61e5e5e589c38736430
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-26 12:34:37 +0200

    echo working for single argument

--- src/builtin/chdir.c
+++ src/builtin/chdir.c
@@ -6,7 +6,7 @@
 /*   By: djonker <djonker@student.codam.nl>         //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2023/05/18 17:21:02 by djonker      /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/08/26 11:49:41 by djonker      \___)=(___/                 */
+/*   Updated: 2023/08/26 12:34:16 by djonker      \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 int	ft_chdir(t_cmds cmds)
 {
 	char	*line;
-	int		i;
 	char	*cwd;
 
 	cwd = malloc(512);
 	getcwd(cwd, 512);
-	i = 0;
 	if (!ft_strncmp(cmds.arguments[1],  "~\0", 2) || !cmds.arguments[1])
 		line = ft_gethome(*cmds.envp);
 	else if (!ft_strncmp(cmds.arguments[1],  "-\0", 2))
