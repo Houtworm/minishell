f419523f360c03d195e46e7afe9f915e3cd2912a
Danny Jonker <djonker@f1r1s6.codam.nl>
2023-08-26 13:40:06 +0200

    echo working perfectly with recursion :)

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,51 +6,11 @@
 /*   By: houtworm <codam@houtworm.net>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 18:13:11 by houtworm          #+#    #+#             */
-/*   Updated: 2023/08/26 13:38:45 by djonker      \___)=(___/                 */
+/*   Updated: 2023/08/26 13:40:03 by djonker      \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
-/*int	ft_echo(t_cmds cmds)*/
-/*{*/
-	/*char	*print;*/
-	/*int		i;*/
-	/*int		j;*/
-	/*int		flag;*/
-
-	/*i = 0;*/
-	/*j = 0;*/
-	/*flag = 0;*/
-	/*if (cmds.arguments[1])*/
-	/*{*/
-		/*print = ft_calloc(512, 1);*/
-		/*if (cmds.arguments[1][0] == '-')*/
-		/*{*/
-			/*i++;*/
-			/*while (cmds.arguments[1][i] == 'n')*/
-				/*i++;*/
-			/*if (cmds.arguments[1][i] == '\0')*/
-				/*flag = 1;*/
-			/*else*/
-			/*{*/
-				/*i = 0;*/
-				/*while (cmds.arguments[1][i] != '\0')*/
-				/*{*/
-					/*print[i] = cmds.arguments[1][i];*/
-					/*i++;*/
-				/*}*/
-				/*print[i] = '\0';*/
-			/*}*/
-		/*}*/
-		/*ft_printf("%s", print);*/
-		/*if (flag == 0)*/
-			/*ft_printf("\n");*/
-	/*}*/
-	/*else*/
-		/*ft_printf("\n");*/
-	/*return (0);*/
-/*}*/
 
 int ft_echorecursion(t_cmds cmds, char *print, int i, int j)
 {
@@ -115,5 +75,3 @@ int	ft_echo(t_cmds cmds)
 		ft_printf("\n");
 	return (0);
 }
-
-// make it recursive damnit
