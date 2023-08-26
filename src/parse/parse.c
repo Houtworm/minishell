3217ca437d9a42ee3e9ea54dd318458f0142e0e7
Yuka Itoh <yitoh@f1r1s7.codam.nl>
2023-08-26 13:15:57 +0200

    test

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: djonker <djonker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/19 04:36:04 by djonker       #+#    #+#                 */
-/*   Updated: 2023/08/26 12:36:31 by yitoh         ########   odam.nl         */
+/*   Updated: 2023/08/26 13:12:28 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ int	*ft_parseline(char *line, t_shell *shell)
 	// printf("split by spchr %s\n", shell->forks[1].cmds[0].absolute);
 	// printf("split by spchr %s\n", shell->forks[1].cmds[1].absolute);
 	// printf("split by spchr %s\n", shell->forks[0].cmds[2].absolute);
-
+	ft_check_redirect(shell->forks->cmds, i);
 	printf("end of testing\n");
 	exit(0);
 //*/
