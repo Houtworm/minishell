db250eb5b3df0ed2b1a0c8a3f46349f4739c94ff
djonker <djonker@student.codam.nl>
2023-09-04 03:09:13 +0200

    cleaned up some more

--- src/parse/globs.c
+++ src/parse/globs.c
@@ -6,7 +6,7 @@
 /*   By: houtworm <codam@houtworm.net>              //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2023/09/03 09:12:54 by houtworm     /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/09/04 02:51:36 by houtworm     \___)=(___/                 */
+/*   Updated: 2023/09/04 03:04:22 by houtworm     \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,77 +119,3 @@ int	ft_parseglobs(t_cmds *cmd)
 	}
 	return (0);
 }
-
-/*int	ft_parseglobs(t_cmds *cmd)*/
-/*{*/
-	/*t_globs			*globs;*/
-	/*int				ipipe;*/
-	/*int				igend;*/
-	/*int				igbeg;*/
-	/*int				i;*/
-
-	/*ipipe = 0;*/
-	/*igbeg = 0;*/
-	/*igend = 0;*/
-	/*i = 0;*/
-	/*globs = ft_initglobstruct(cmd->pipeline);*/
-	/*while (globs->pipeline[ipipe + igbeg + igend + i])*/
-	/*{*/
-		/*igend = 0;*/
-		/*if (globs->pipeline[ipipe + igbeg] == '\'')*/
-		/*{*/
-			/*igbeg++;*/
-			/*while (globs->pipeline[ipipe + igbeg] != '\'' && globs->pipeline[ipipe + igbeg])*/
-			/*{*/
-				/*globs->gstart[igbeg] = globs->pipeline[ipipe + igbeg];*/
-				/*igbeg++;*/
-			/*}*/
-			/*igbeg++;*/
-		/*}*/
-		/*if (globs->pipeline[ipipe + igbeg] == '\"')*/
-		/*{*/
-			/*igbeg++;*/
-			/*while (globs->pipeline[ipipe + igbeg] != '\"' && globs->pipeline[ipipe + igbeg])*/
-			/*{*/
-				/*globs->gstart[igbeg] = globs->pipeline[ipipe + igbeg];*/
-				/*igbeg++;*/
-			/*}*/
-			/*igbeg++;*/
-		/*}*/
-		/*if (globs->pipeline[ipipe + igbeg] == ' ')*/
-		/*{*/
-			/*ipipe = ipipe + igbeg + 1;*/
-			/*igbeg = 0;*/
-		/*}*/
-		/*if (globs->pipeline[ipipe + igbeg] == '*')*/
-		/*{*/
-			/*globs->period = 0;*/
-			/*if (globs->pipeline[ipipe + igbeg - 1] == '.')*/
-				/*globs->period = 1;*/
-			/*globs->gstart[igbeg] = '\0';*/
-			/*igbeg++;*/
-			/*while (globs->pipeline[ipipe + igbeg + igend] && !ft_strchr(" /", globs->pipeline[ipipe + igbeg + igend]))*/
-			/*{*/
-				/*globs->gend[igend] = globs->pipeline[ipipe + igbeg + igend];*/
-				/*igend++;*/
-			/*}*/
-			/*globs->gend[igend] = '\0';*/
-			/*if (globs->pipeline[ipipe + igbeg + igend] == '/')*/
-			/*{*/
-				/*i = 0;*/
-				/*while (globs->pipeline[ipipe + igbeg + igend + i] && globs->pipeline[ipipe + igbeg + igend + i] != ' ')*/
-				/*{*/
-					/*globs->subdir[i] = globs->pipeline[ipipe + igbeg + igend + i];*/
-					/*i++;*/
-				/*}*/
-				/*globs->subdir[i] = '\0';*/
-			/*}*/
-			/*globs->start = ft_substr(globs->pipeline, 0, ipipe);*/
-			/*ft_parsewildcard(*cmd, globs);*/
-			/*cmd->pipeline = ft_vastrjoin(3, globs->start, globs->matches, &cmd->pipeline[ipipe + igbeg + igend + i]);*/
-		/*}*/
-		/*globs->gstart[igbeg] = globs->pipeline[ipipe + igbeg + igend];*/
-		/*igbeg++;*/
-	/*}*/
-	/*return (0);*/
-/*}*/
