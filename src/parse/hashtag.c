0134eefa6600ad4aa3e32f6f843ffc628fbd0404
Yuka Itoh <yitoh@f1r1s5.codam.nl>
2023-09-06 18:06:31 +0200

    hashtag fixed

--- src/parse/hashtag.c
+++ src/parse/hashtag.c
@@ -22,31 +22,27 @@ char	*ft_parsehashtag(char *line)
 		i = 0;
 		if (line[0] == '#')
 			line[0] = '\0';
-		while (line[i] && (line[i] != ' ' || line[i - 1] != '#'))
+		while (line[i] && (line[i] != ' ' || line[i + 1] != '#'))
 		{
 			if (line[i] == '\'')
 			{
 				i++;
 				while (line[i] != '\'')
 					i++;
-				i++;
 			}
 			if (line[i] == '\"')
 			{
 				i++;
 				while (line[i] != '\"')
 					i++;
-				i++;
 			}
-		// printf ("line[%d] = %c\n", i, line[i]);
 			i++;
 		}
 		/*temp = ft_substr(line, 0, i);*/
 		/*free(line);*/
 		/*line = ft_strdup(temp);*/
 		/*free(temp);*/
-		line = ft_substr(line, 0, i - 1); // memory leak, but we need to fix a bug in the parser for this.
+		line = ft_substr(line, 0, i); // memory leak, but we need to fix a bug in the parser for this.
 	}
-		// printf ("line = %s\n", line);
 	return (line);
 }
