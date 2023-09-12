5a1001cdb9b5c34d097d53459d312760997579e6
djonker <djonker@student.codam.nl>
2023-09-12 18:14:04 +0200

    builtin fixes

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: houtworm <codam@houtworm.net>              //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2023/09/12 15:11:33 by houtworm     /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/09/12 18:04:19 by houtworm     \___)=(___/                 */
+/*   Updated: 2023/09/12 18:12:34 by houtworm     \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int	ft_builtincheck(t_cmds cmds, int cmdnbr, int forknbr, t_shell *shell)
 	if (!ft_strncmp(cmds.arguments[0], "exit\0", 5))
 		ft_exit(cmds);
 	else if (!ft_strncmp(cmds.arguments[0], "cd\0", 3))
-		ret = ft_chdir(cmds);
+		return (ft_chdir(cmds));
 	else if (ret)
 		return (ft_executebuiltin(cmds, cmdnbr, forknbr, shell, ret));
 	return (-1111);
