72fffa512fa97e62821c17c3d412a59e624997a8
Danny Jonker <djonker@f1r1s2.codam.nl>
2023-09-15 15:49:23 +0200

    added function pointer for builtins

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -6,39 +6,11 @@
 /*   By: houtworm <codam@houtworm.net>              //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2023/09/12 15:11:33 by houtworm     /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/09/15 15:40:11 by djonker      \___)=(___/                 */
+/*   Updated: 2023/09/15 15:48:38 by djonker      \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
-int	ft_executebuiltin(t_cmds cmds, int cmdnbr, int forknbr, t_shell *shell, int ret)
-{
-	int	pid;
-	int	code;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		ft_dupmachine(cmds, cmdnbr, forknbr, shell);
-		if (ret == 1)
-			exit (ft_echo(cmds));
-		else if (ret == 2)
-			exit (ft_echo(cmds));
-		else if (ret == 3)
-			exit (ft_env(cmds));
-		else if (ret == 4)
-			exit (ft_export(cmds));
-		else if (ret == 5)
-			exit (ft_pwd(cmds));
-		else if (ret == 6)
-			exit (ft_unset(cmds));
-	}
-	waitpid(pid, &code, 0);
-	code = WEXITSTATUS(code);
-	shell->code = code;
-	return (code);
-}
 
 struct	builtin
 {
@@ -48,6 +20,7 @@ struct	builtin
 
 int	ft_builtincheck(t_cmds cmds, int cmdnbr, int forknbr, t_shell *shell)
 {
+	int	pid;
 	int	ret;
 	int i;
 	struct builtin	bui[5] = {
@@ -62,15 +35,23 @@ int	ft_builtincheck(t_cmds cmds, int cmdnbr, int forknbr, t_shell *shell)
 	while (i < 5)
 	{
 		if (!ft_strncmp(cmds.arguments[0], bui[i].compare, ft_strlen(bui[i].compare) + 1))
-			exit (bui[i].func(cmds));
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				ft_dupmachine(cmds, cmdnbr, forknbr, shell);
+				exit (bui[i].func(cmds));
+			}
+			waitpid(pid, &ret, 0);
+			ret = WEXITSTATUS(ret);
+			shell->code = ret;
+			return (ret);
+		}
 		i++;
 	}
-	ret = 0;
 	if (!ft_strncmp(cmds.arguments[0], "exit\0", 5))
 		ft_exit(cmds);
 	else if (!ft_strncmp(cmds.arguments[0], "cd\0", 3))
 		return (ft_chdir(cmds));
-	else if (ret)
-		return (ft_executebuiltin(cmds, cmdnbr, forknbr, shell, ret));
 	return (-1111);
 }
