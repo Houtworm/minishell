179f38f997a48d3c5281e1eeb9a6e2067c7d985b
djonker <djonker@student.codam.nl>
2023-10-16 14:51:52 +0200

    normed history

--- src/main/history.c
+++ src/main/history.c
@@ -6,55 +6,69 @@
 /*   By: houtworm <codam@houtworm.net>              //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2023/10/01 02:06:31 by houtworm     /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/10/15 05:38:53 by houtworm     \___)=(___/                 */
+/*   Updated: 2023/10/16 14:32:52 by houtworm     \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	ft_writehistory(char *line, char *file)
+void	ft_addlinetohistory(char *line, char *file)
 {
-		int		histfd;
-		int		tempfd;
-		char	*gnl;
+	int		histfd;
+	int		tempfd;
+	char	*gnl;
 
-		rl_clear_history();
-		histfd = open(file, O_RDONLY);
-		tempfd = open("/tmp/minishell/history.tmp", O_RDWR | O_CREAT | O_TRUNC, 0666);
-		while (get_next_line(histfd, &gnl) > 0)
-		{
-			if (ft_strncmp(line, gnl, 500))
-				ft_putendl_fd(gnl, tempfd);
-			free(gnl);
-		}
+	histfd = open(file, O_RDONLY);
+	tempfd = open("/tmp/minishell/hist.tmp", O_RDWR | O_CREAT | O_TRUNC, 0666);
+	while (get_next_line(histfd, &gnl) > 0)
+	{
+		if (ft_strncmp(line, gnl, 500))
+			ft_putendl_fd(gnl, tempfd);
 		free(gnl);
-		ft_putendl_fd(line, tempfd);
-		close(tempfd);
-		close(histfd);
-		tempfd = open("/tmp/minishell/history.tmp", O_RDONLY);
-		histfd = open(file, O_RDWR | O_CREAT | O_TRUNC, 0666);
-		while (get_next_line(tempfd, &gnl) > 0)
-		{
-			ft_putendl_fd(gnl, histfd);
-			free(gnl);
-		}
+	}
+	free(gnl);
+	ft_putendl_fd(line, tempfd);
+	close(tempfd);
+	close(histfd);
+}
+
+void	ft_writenewhistory(char *file)
+{
+	int		histfd;
+	int		tempfd;
+	char	*gnl;
+
+	tempfd = open("/tmp/minishell/hist.tmp", O_RDONLY);
+	histfd = open(file, O_RDWR | O_CREAT | O_TRUNC, 0666);
+	while (get_next_line(tempfd, &gnl) > 0)
+	{
+		ft_putendl_fd(gnl, histfd);
 		free(gnl);
-		close(tempfd);
-		close(histfd);
-		ft_readhistory(file);
+	}
+	free(gnl);
+	close(tempfd);
+	close(histfd);
 }
 
 void	ft_readhistory(char *file)
 {
-		int		histfd;
-		char	*line;
+	int		histfd;
+	char	*line;
 
-		histfd = open(file, O_RDONLY);
-		while (get_next_line(histfd, &line) > 0)
-		{
-			add_history(line);
-			free(line);
-		}
+	histfd = open(file, O_RDONLY);
+	while (get_next_line(histfd, &line) > 0)
+	{
+		add_history(line);
 		free(line);
-		close(histfd);
+	}
+	free(line);
+	close(histfd);
+}
+
+void	ft_writehistory(char *line, char *file)
+{
+	rl_clear_history();
+	ft_addlinetohistory(line, file);
+	ft_writenewhistory(file);
+	ft_readhistory(file);
 }
