5473874b87cbc53e39f6c12192a692a7d33eaf79
djonker <djonker@student.codam.nl>
2023-10-18 04:40:34 +0200

    chdir.c is norm :)

--- src/builtin/chdir.c
+++ src/builtin/chdir.c
@@ -6,11 +6,31 @@
 /*   By: djonker <djonker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/18 17:21:02 by djonker       #+#    #+#                 */
-/*   Updated: 2023/10/17 16:56:40 by houtworm     \___)=(___/                 */
+/*   Updated: 2023/10/18 04:38:01 by houtworm     \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	ft_writezfile(char *home, char *tmpfile)
+{
+	int		mshzfd;
+	int		tempfd;
+	char	*line;
+
+	tempfd = open(tmpfile, O_RDONLY);
+	mshzfd = open(home, O_RDWR | O_CREAT | O_TRUNC, 0666);
+	while (get_next_line(tempfd, &line) > 0)
+	{
+		ft_putendl_fd(line, mshzfd);
+		free(line);
+	}
+	free(line);
+	free(home);
+	free(tmpfile);
+	close(mshzfd);
+	close(tempfd);
+}
 
 void	ft_adddirtoz(char *cwd, t_shell *msh)
 {
@@ -36,45 +56,13 @@ void	ft_adddirtoz(char *cwd, t_shell *msh)
 	free(line);
 	close(tempfd);
 	close(mshzfd);
-	tempfd = open(tmpfile, O_RDONLY);
-	mshzfd = open(home, O_RDWR | O_CREAT | O_TRUNC, 0666);
-	while (get_next_line(tempfd, &line) > 0)
-	{
-		ft_putendl_fd(line, mshzfd);
-		free(line);
-	}
-	free(line);
-	free(home);
-	free(tmpfile);
-	close(mshzfd);
-	close(tempfd);
+	ft_writezfile(home, tmpfile);
 }
 
-int	ft_chdir(t_commands cmd, t_shell *msh)
+int	ft_cdgetoldcwd(t_commands cmd, t_shell *msh, char *line)
 {
-	char	*line;
-	char	*newcwd;
 	char	*oldcwd;
 
-	if (cmd.arg[2])
-		return (ft_errorret("too many arguments", "cd", 1));
-	if (!cmd.arg[1] || cmd.arg[1][0] == '\0')
-	{
-		newcwd = ft_getuser(msh->envp);
-		line = ft_vastrjoin(3, "/home/", newcwd, "/");
-		free(newcwd);
-	}
-	else if (!ft_strncmp(cmd.arg[1],  "-\0", 2))
-	{
-		line = ft_getenvval(msh->envp, "OLDPWD");
-		if (!line)
-			return (ft_errorret("OLDPWD not set", "cd", 1));
-		printf("%s\n", line);
-	}
-	else
-	{
-		line = ft_strdup(cmd.arg[1]);
-	}
 	oldcwd = malloc(512);
 	getcwd(oldcwd, 512);
 	if (chdir(line))
@@ -91,6 +79,14 @@ int	ft_chdir(t_commands cmd, t_shell *msh)
 	if (oldcwd[0] == '/')
 		ft_setenv(msh->envp, "OLDPWD", oldcwd);
 	free(oldcwd);
+	return (0);
+}
+
+void	ft_cdgetnewcwd(t_commands cmd, t_shell *msh)
+{
+	char	*newcwd;
+	char	*line;
+
 	newcwd = malloc(512);
 	getcwd(newcwd, 512);
 	if (newcwd[0] != '/')
@@ -106,5 +102,31 @@ int	ft_chdir(t_commands cmd, t_shell *msh)
 	ft_adddirtoz(newcwd, msh);
 	free(newcwd);
 	ft_charpptofd(msh->envp, msh);
+}
+int	ft_chdir(t_commands cmd, t_shell *msh)
+{
+	char	*line;
+	char	*temp;
+
+	if (cmd.arg[2])
+		return (ft_errorret("too many arguments", "cd", 1));
+	if (!cmd.arg[1] || cmd.arg[1][0] == '\0')
+	{
+		temp = ft_getuser(msh->envp);
+		line = ft_vastrjoin(3, "/home/", temp, "/");
+		free(temp);
+	}
+	else if (!ft_strncmp(cmd.arg[1],  "-\0", 2))
+	{
+		line = ft_getenvval(msh->envp, "OLDPWD");
+		if (!line)
+			return (ft_errorret("OLDPWD not set", "cd", 1));
+		printf("%s\n", line);
+	}
+	else
+		line = ft_strdup(cmd.arg[1]);
+	if (ft_cdgetoldcwd(cmd, msh, line))
+		return (1);
+	ft_cdgetnewcwd(cmd, msh);
 	return (0);
 }
