2161766e72a80e8cf29062662b509b5b558221c7
Yuka Itoh <yitoh@f0r1s12.codam.nl>
2023-10-18 23:10:39 +0200

    variable.c still need to be normed

--- src/parse/variable.c
+++ src/parse/variable.c
@@ -6,11 +6,84 @@
 /*   By: houtworm <codam@houtworm.net>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/27 08:14:18 by houtworm      #+#    #+#                 */
-/*   Updated: 2023/10/18 17:01:14 by houtworm      ########   odam.nl         */
+/*   Updated: 2023/10/18 23:03:57 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+char	*ft_quoteinvariable(char *line, char *begin, int *i, int *j)
+{
+	begin[*j] = line[*i];
+	(*i)++;
+	(*j)++;
+	if (line[(*i) - 1] == '\'')
+	{
+		while (line[*i] && line[*i] != '\'')
+		{
+			begin[*j] = line[*i];
+			(*i)++;
+			(*j)++;
+		}
+	}
+	if (line[(*i) - 1] == '\"')
+	{
+		while (line[*i] && line[*i] != '\"')
+		{
+			if (line[*i] == '$' && line[(*i) + 1] != '\''
+				&& line[*i + 1] != '\"' && line[*i + 1] != '(')
+				break ;
+			begin[*j] = line[*i];
+			(*i)++;
+			(*j)++;
+		}
+	}
+	return (begin);
+}
+
+char	*ft_cpnonvariable(char *line, char *begin, int *i, int *j)
+{
+	begin[*j] = line[*i];
+	(*i)++;
+	(*j)++;
+	if (line[*i] == '$' && line[(*i) + 1] == '(')
+	{
+		while (line[*i] != ')')
+		{
+			begin[*j] = line[*i];
+			(*i)++;
+			(*j)++;
+		}
+		begin[*j] = line[*i];
+		(*i)++;
+		(*j)++;
+	}
+	return (begin);
+}
+
+char	*ft_foundvariable(t_shell msh, char *line, int *i)
+{
+	char	*val;
+
+	if (line[*i] == '?')
+	{
+		val = ft_itoa(msh.code % 256);
+		(*i)++;
+	}
+	else if (line[*i] == '$')
+	{
+		val = ft_itoa(msh.pid);
+		(*i)++;
+	}
+	else if (line[*i] == ' ')
+	{
+		val = ft_strdup("$");
+		(*i)++;
+	}
+	else if (!line[*i])
+		val = ft_strdup("$");
+	return (val);
+}
 
 char	*ft_parsevariable(char *line, t_shell msh, int quote)
 {
@@ -30,72 +103,20 @@ char	*ft_parsevariable(char *line, t_shell msh, int quote)
 		j = 0;
 		while (line[i] && line[i] != '$')
 		{
-			if (quote && line[i] == '\'')
-			{
-				begin[j] = line[i];
-				i++;
-				j++;
-				while (line[i] && line[i] != '\'')
-				{
-					begin[j] = line[i];
-					i++;
-					j++;
-				}
-			}
-			if (quote && line[i] == '\"')
-			{
-				begin[j] = line[i];
-				i++;
-				j++;
-				while (line[i] && line[i] != '\"')
-				{
-					if (line[i] == '$' && line[i + 1] != '\'' && line[i + 1] != '\"' && line[i + 1] != '(')
-						break ;
-					begin[j] = line[i];
-					i++;
-					j++;
-				}
-			}
-			if (!line[i] || (line[i] == '$' && line[i + 1] != '\'' && line[i + 1] != '\"' && line[i + 1] != '('))
+			if (quote && (line[i] == '\'' || line[i] == '\"'))
+				begin = ft_quoteinvariable(line, begin, &i, &j);
+			if (!line[i] || (line[i] == '$' && line[i + 1] != '\''
+					&& line[i + 1] != '\"' && line[i + 1] != '('))
 				break ;
-			begin[j] = line[i];
-			i++;
-			j++;
-			if (line[i] == '$' && line[i + 1] == '(')
-			{
-				while (line[i] != ')')
-				{
-					begin[j] = line[i];
-					j++;
-					i++;
-				}
-				begin[j] = line[i];
-				i++;
-				j++;
-			}
+			begin = ft_cpnonvariable(line, begin, &i, &j);
 		}
 		if (line[i] == '$')
 		{
 			begin[j] = '\0';
 			i++;
 		}
-		if (line[i] == '?')
-		{
-			val = ft_itoa(msh.code % 256);
-			i++;
-		}
-		else if (line[i] == '$')
-		{
-			val = ft_itoa(msh.pid);
-			i++;
-		}
-		else if (line[i] == ' ')
-		{
-			val = ft_strdup("$");
-			i++;
-		}
-		else if (!line[i])
-			val = ft_strdup("$");
+		if (!line[i] || ft_strchr("?$ ", line[i]))
+			val = ft_foundvariable(msh, line, &i);
 		else
 		{
 			j = 0;
