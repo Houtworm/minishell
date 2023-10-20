9e6a4b215cea2b1feab293a1c0ec23bc85f0edb8
djonker <djonker@student.codam.nl>
2023-10-20 18:03:42 +0200

    progress in globbing

--- src/globs/globsub.c
+++ src/globs/globsub.c
@@ -6,7 +6,7 @@
 /*   By: houtworm <codam@houtworm.net>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/20 03:29:24 by houtworm      #+#    #+#                 */
-/*   Updated: 2023/10/20 17:38:17 by houtworm      ########   odam.nl         */
+/*   Updated: 2023/10/20 18:02:25 by houtworm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_subjustaslash(t_globs *globs, char *dname, int i)
 {
-	char			*temp;
-	char			*temp2;
+	char	*temp;
+	char	*temp2;
 
 	temp = ft_cpptostr(globs->tempsubdir);
 	if (i == 0)
@@ -27,12 +27,66 @@ int	ft_subjustaslash(t_globs *globs, char *dname, int i)
 	return (1);
 }
 
+void	ft_subfoundglob(t_globs *globs, int i, int j, char *fullpath, struct dirent *dirents, char *dname)
+{
+	char	*temp;
+	char	*temp2;
+
+	if (ft_firstsubglob(globs, dirents, i, j))
+	{
+		free(globs->tempsubdir[i]);
+		globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
+		if (!globs->subdir[i + 1])
+		{
+			temp = ft_cpptostr(globs->tempsubdir);
+			temp2 = ft_vastrjoin(3, globs->pardir, dname, temp);
+			ft_addglobmatch(globs, temp2);
+			ft_vafree(2, temp, temp2);
+		}
+		else if (dirents->d_type == DT_DIR)
+		{
+			free(globs->tempsubdir[i]);
+			globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
+			temp = ft_vastrjoin(3, fullpath, "/", dirents->d_name);
+			globs->temptype = dirents->d_type;
+			ft_recursivematchsub(globs, temp, dname, i + 1);
+			free(temp);
+		}
+	}
+}
+
+void	ft_subatend(t_globs *globs, int i, int offset, char *fullpath, struct dirent *dirents, char *dname)
+{
+	char	*temp;
+	char	*temp2;
+
+	if (!globs->subdir[i + offset])
+	{
+		free(globs->tempsubdir[i]);
+		globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
+		temp = ft_cpptostr(globs->tempsubdir);
+		temp2 = ft_vastrjoin(3, globs->pardir, dname, temp);
+		ft_addglobmatch(globs, temp2);
+		ft_vafree(2, temp, temp2);
+	}
+	else
+	{
+		if (dirents->d_type == DT_DIR)
+		{
+			free(globs->tempsubdir[i]);
+			globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
+			temp = ft_vastrjoin(3, fullpath, "/", dirents->d_name);
+			globs->temptype = dirents->d_type;
+			ft_recursivematchsub(globs, temp, dname, i + 1);
+			free(temp);
+		}
+	}
+}
+
 void	ft_subcomparedir(t_globs *globs, struct dirent *dirents, int i, char *fullpath, char *dname)
 {
 	int		offset;
 	int		j;
-	char			*temp;
-	char			*temp2;
 
 	offset = 1;
 	if ((globs->subdir[i][1] == '.' && dirents->d_name[0] == '.') || (globs->subdir[i][1] != '.' && dirents->d_name[0] != '.'))
@@ -41,65 +95,14 @@ void	ft_subcomparedir(t_globs *globs, struct dirent *dirents, int i, char *fullp
 		while (globs->subdir[i][j + offset] == '\\' || (dirents->d_name[j] && dirents->d_name[j] == globs->subdir[i][j + offset]))
 		{
 			if (globs->subdir[i][j + offset] == '\\')
-			{
 				offset++;
-			}
 			else
-			{
 				j++;
-			}
 		}
 		if (globs->subdir[i][j + offset] && globs->subdir[i][j + offset - 1] != '\\' && ft_strchr("*?[", globs->subdir[i][j + offset]))
-		{
-			if (ft_firstsubglob(globs, dirents, i, j + offset))
-			{
-				free(globs->tempsubdir[i]);
-				globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
-				if (!globs->subdir[i + 1])
-				{
-					temp = ft_cpptostr(globs->tempsubdir);
-					temp2 = ft_vastrjoin(3, globs->pardir, dname, temp);
-					ft_addglobmatch(globs, temp2);
-					ft_vafree(2, temp, temp2);
-				}
-				else
-				{
-					if (dirents->d_type == DT_DIR)
-					{
-						free(globs->tempsubdir[i]);
-						globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
-						temp = ft_vastrjoin(3, fullpath, "/", dirents->d_name);
-						globs->temptype = dirents->d_type;
-						ft_recursivematchsub(globs, temp, dname, i + 1);
-						free(temp);
-					}
-				}
-			}
-		}
+			ft_subfoundglob(globs, i, j + offset, fullpath, dirents, dname);
 		if (globs->subdir[i][j + offset] == '\0' && dirents->d_name[j] == '\0')
-		{
-			if (!globs->subdir[i + offset])
-			{
-				free(globs->tempsubdir[i]);
-				globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
-				temp = ft_cpptostr(globs->tempsubdir);
-				temp2 = ft_vastrjoin(3, globs->pardir, dname, temp);
-				ft_addglobmatch(globs, temp2);
-				ft_vafree(2, temp, temp2);
-			}
-			else
-			{
-				if (dirents->d_type == DT_DIR)
-				{
-					free(globs->tempsubdir[i]);
-					globs->tempsubdir[i] = ft_strjoin("/", dirents->d_name);
-					temp = ft_vastrjoin(3, fullpath, "/", dirents->d_name);
-					globs->temptype = dirents->d_type;
-					ft_recursivematchsub(globs, temp, dname, i + 1);
-					free(temp);
-				}
-			}
-		}
+			ft_subatend(globs, i, offset, fullpath, dirents, dname);
 	}
 }
 
