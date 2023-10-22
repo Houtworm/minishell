81abda4c6077be3458f743604bc2849745c16d64
Yuka Itoh <yitoh@f0r1s10.codam.nl>
2023-10-22 04:58:23 +0200

    verify.c normed

--- src/exec/verify.c
+++ src/exec/verify.c
@@ -6,7 +6,7 @@
 /*   By: houtworm <codam@houtworm.net>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/25 04:55:07 by houtworm      #+#    #+#                 */
-/*   Updated: 2023/10/19 06:41:54 by djonker       ########   odam.nl         */
+/*   Updated: 2023/10/22 04:54:16 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,68 +32,76 @@ int	ft_checkinputfile(char *file)
 	return (0);
 }
 
+int	ft_accesserror(char *file, int i)
+{
+	if (ft_chrstr('/', file))
+	{
+		while (i > 0 && file[i] != '/')
+		{
+			file[i] = '\0';
+			i--;
+		}
+		file[i] = '\0';
+		if (access(file, F_OK) < 0)
+			return (ft_errorret("no such file or directory", file, 1));
+	}
+	else
+		return (ft_errorret("is a directory", file, 1));
+	return (0);
+}
+
 int	ft_checkoutputfile(char *file)
 {
 	int		i;
 	int		fd;
 
 	i = ft_strlen(file) - 1;
-	if (file)
+	if (!file)
+		return (0);
+	if (file[0] == '\0')
+		return (ft_errorret("no such file or directory", file, 1));
+	fd = open(file, O_RDONLY | O_CREAT);
+	close(fd);
+	if (access(file, F_OK) < 0)
+		return (ft_accesserror(file, i));
+	else if (access(file, W_OK) < 0)
+		return (ft_errorret("permission denied", file, 1));
+	else if (fd == -1)
+		return (ft_errorret("is a directory", file, 1));
+	return (0);
+}
+
+int	ft_joinpathsarg(char **args, char **paths, char **temp, int *i)
+{
+	if (args[0] && ft_chrstr('/', args[0]))
+		*temp = ft_strjoin(NULL, args[0]);
+	else
 	{
-		if (file[0] == '\0')
-			return (ft_errorret("no such file or directory", file, 1));
-		fd = open(file, O_RDONLY | O_CREAT);
-		close(fd);
-		if (access(file, F_OK) < 0)
-		{
-			if (ft_chrstr('/', file))
-			{
-				while (i > 0 && file[i] != '/')
-				{
-					file[i] = '\0';
-					i--;
-				}
-				file[i] = '\0';
-				if (access(file, F_OK) < 0)
-					return (ft_errorret("no such file or directory", file, 1));
-			}
-			else
-				return (ft_errorret("is a directory", file, 1));
-		}
-		else if (access(file, W_OK) < 0)
-		{
-			return (ft_errorret("permission denied", file, 1));
-		}
-		else if (fd == -1)
-			return (ft_errorret("is a directory", file, 1));
+		if (!paths)
+			return (ft_errorret("command not found", args[0], 127));
+		*temp = ft_strjoin(paths[*i], args[0]);
+	}
+	while (access(*temp, F_OK) && paths[*i] && !ft_chrstr('/', args[0]))
+	{
+		free(*temp);
+		(*i)++;
+		*temp = ft_strjoin(paths[*i], args[0]);
 	}
 	return (0);
 }
 
 int	ft_checkcommand(char **args, char **envp)
-
 {
 	int		i;
+	int		returncode;
 	char	*temp;
 	char	**paths;
 
 	i = 0;
 	paths = ft_getpaths(envp, 1);
-	if (args[0] && ft_chrstr('/', args[0]))
-		temp = ft_strjoin(NULL, args[0]);
-	else
-	{
-		if (!paths)
-			return (ft_errorret("command not found", args[0], 127));
-		temp = ft_strjoin(paths[i], args[0]);
-	}
-	while (access(temp, F_OK) && paths[i] && !ft_chrstr('/', args[0]))
-	{
-		free(temp);
-		i++;
-		temp = ft_strjoin(paths[i], args[0]);
-	}
-	if (!paths[i] || ft_isallbyte(args[0], ' ') || ft_isallbyte(args[0], '.') || access(temp, F_OK))
+	returncode = ft_joinpathsarg(args, paths, &temp, &i);
+	if (!paths[i] || ft_isallbyte(args[0], ' ')
+		|| ft_isallbyte(args[0], '.') || access(temp, F_OK))
 	{
 		free (temp);
 		ft_frearr(paths);
@@ -106,5 +114,5 @@ int	ft_checkcommand(char **args, char **envp)
 		return (ft_errorret("permission denied", args[0], 126));
 	}
 	free (temp);
-	return (0);
+	return (returncode);
 }
