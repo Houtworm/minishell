31a7b9b8363e91a24b3afb4b67862728fa51c196
djonker <djonker@student.codam.nl>
2023-10-23 13:03:59 +0200

    anyof.c is now norm :)

--- src/globs/anyof.c
+++ src/globs/anyof.c
@@ -6,7 +6,7 @@
 /*   By: houtworm <codam@houtworm.net>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/20 00:51:38 by houtworm      #+#    #+#                 */
-/*   Updated: 2023/10/23 12:49:28 by houtworm      ########   odam.nl         */
+/*   Updated: 2023/10/23 13:02:35 by houtworm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,55 +64,24 @@ int	ft_nextanyof(t_globs *globs, char *dname, int ri, int gi)
 int	ft_firstanyof(t_globs *globs, char *dname, int ri)
 {
 	int		gi;
-	char	*temp;
 
 	gi = 0;
-	if (!globs->anyof[0])
-	{
-		while (globs->gend[gi] && globs->gend[gi] != ']')
-		{
-			globs->glob[gi + 1] = globs->gend[gi];
-			globs->anyof[gi] = globs->gend[gi];
-			gi++;
-		}
-		if (!globs->gend[gi])
-			return (0);
-		globs->anyof[gi] = '\0';
-		globs->glob[gi + 1] = ']';
-		temp = ft_substr(globs->gend, gi + 1, ft_strlen(globs->gend));
-		free(globs->gend);
-		globs->gend = ft_strdup(temp);
-		free(temp);
-	}
-	else
-	{
-		while (globs->glob[gi + 1] && globs->glob[gi + 1] != ']')
-		{
-			globs->anyof[gi] = globs->glob[gi + 1];
-			gi++;
-		}
-		if (!globs->glob[gi + 1])
-			return (0);
-		globs->anyof[gi] = '\0';
-	}
-	if (globs->anyof[0] == '\0')
+	gi = ft_matchanyof(globs->gend, ri, gi, dname[ri]);
+	if (gi == 0)
 		return (0);
-	if (ft_strchr(globs->anyof, dname[ri]))
-	{
-		gi = 0;
+	else
 		ri++;
-		while (globs->gend[gi] == '\\' || (dname[ri] && globs->gend[gi] && dname[ri] == globs->gend[gi]))
-		{
-			if (globs->gend[gi] != '\\')
-				ri++;
-			gi++;
-		}
-		if (dname[ri] == '\0' && globs->gend[gi] == '\0')
-			return (1);
-		if (dname[ri] == '\0' || globs->gend[gi] == '\0')
-			return (0);
-		if (ft_strchr("*?[", globs->gend[gi]))
-			return (ft_nextglob(globs, dname, ri, gi));
+	while (globs->gend[gi] == '\\' || (dname[ri] && globs->gend[gi] && dname[ri] == globs->gend[gi]))
+	{
+		if (globs->gend[gi] != '\\')
+			ri++;
+		gi++;
 	}
+	if (dname[ri] == '\0' && globs->gend[gi] == '\0')
+		return (1);
+	if (dname[ri] == '\0' || globs->gend[gi] == '\0')
+		return (0);
+	if (ft_strchr("*?[", globs->gend[gi]))
+		return (ft_nextglob(globs, dname, ri, gi));
 	return (0);
 }
